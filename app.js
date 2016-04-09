42a7483b66c6aa1bf836b89cd8180517cb134752
Aldwin Pagan-Pedrogo <aldwin.pagan@upr.edu>
2016-04-09 17:09:57 -0400

    Added hogan-express

--- app.js
+++ app.js
@@ -11,8 +11,11 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', require('hogan-express'));
+app.set('view engine','html');
+app.set('layout', 'layout');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
