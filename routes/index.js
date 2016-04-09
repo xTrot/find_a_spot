509ff21c698e9372fa7053772f3b6f4951f77c0f
xtrot <enddyygf93@live.com>
2016-04-09 18:33:47 -0400

    Room data post done w/o DB Query.

--- routes/index.js
+++ routes/index.js
@@ -27,11 +27,20 @@ router.get('/room?', function(req, res, next) {
 //========================================================//
 
 //                  Post New Room Status
-router.post('/room?', function(req, res, next) {
+router.post('/room?', function(err, req, res, next) {
   if(req.query.room_id){
     console.log("Post:"+req.query.room_id);
+    console.log("Body:"+req.body[0].rf_id+","+req.body[0].seats);
+    console.log("Body:"+req.body[1].rf_id+","+req.body[1].seats);
+    res.render('index', { title: 'Express' });
+  }else{
+    err.status = 404;
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {}
+    });
   }
-  res.render('index', { title: 'Express' });
 });
 
 //========================================================//
